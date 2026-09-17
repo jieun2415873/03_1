0c59adf599e90feb6ed9aaf8c157e611bd78ac9e
jieun2415873 <wldms@sookmyung.ac.kr>
2026-09-17 16:35:02 +0900

    03_4

--- main.c
+++ main.c
@@ -2,12 +2,15 @@
 
 int main(void) 
 {
-    char c;
+    int a, b;
+     
+    printf("분자를 입력하세요 : ");
+    scanf("%d", &a);
 
-    printf("enter a character : ");
-    scanf("%c", &c);
+    printf("분모를 입력하세요 : ");
+    scanf("%d", &b);
 
-    printf("the next character of %c (%i) is %c (%i)\n", c, c, c+1, c+1);
+    printf("나누기의 결과는 %f 입니다.\n", (float)a/b);
 
     return 0;
 }
